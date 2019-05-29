7a1495b16a6be23a513bccbd3a1d03d41ccf082f
LijinPengT <1781062962@qq.com>
2019-05-29 21:25:48 +0800

    查看打卡详情

---
+++ pages/punch/punch-detail/punch-detail.js
@@ -0,0 +1,79 @@
+// pages/punch/punch-detail/punch-detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+    data: {
+      dayStyle: [
+        { month: 'current', day: new Date().getDate(), color: 'white', background: '#f25458' },
+        { month: 'prev', day: new Date().getDate(), color: 'white', background: '#ffffff', border:'#f25458' }
+      ],
+    },
+    // 给点击的日期设置一个背景颜色
+    dayClick: function (event) {
+      let clickDay = event.detail.day;
+      let changeDay = `dayStyle[1].day`;
+      let changeBg = `dayStyle[1].background`;
+      this.setData({
+        [changeDay]: clickDay,
+        [changeBg]: "#f25458"
+      })
+    },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
