917f7af25f8ded15e9478b78f63fd2cbe387d03b
CaiyueDamowang <www.caiyue@qq.com>
2019-05-30 19:16:24 +0800

    待办页面逻辑

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,7 @@ const app = getApp()
 
 Page({
   data: {
+    i:0,
     show:true,
     pendList: [
       {
@@ -22,6 +23,15 @@ Page({
     bgBar: 'bgBar',
     punchedBar: 'punchedBar'
   },
+  onLoad: function (options) {
+    var List = this.data.pendList;
+    var i = List.length;
+    console.log(i);
+    this.setData({
+      i:i
+    })
+
+  },
   select: function(e) {
     console.log(e)
     this.setData({
@@ -30,6 +40,18 @@ Page({
   toShow:function(){
     var List = this.data.pendList;
     List.unshift({content:'未定义'})
+    console.log(List);
+    console.log(this.data.pendList);
+    this.setData({
+      pendList:List
+    })
+  },
+  commitValue:function(event){
+    console.log(event.detail.value)
+    var List = this.data.pendList;
+    List.unshift(
+      {content:event.detail.value}
+    )
     this.setData({
       pendList:List
     })
