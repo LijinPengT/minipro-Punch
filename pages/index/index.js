cf7ff44266f21b09fb1ad81cd25ec3e2af6eb3fc
CaiyueDamowang <www.caiyue@qq.com>
2019-05-30 21:18:45 +0800

    弟弟好淡定啊

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,15 +8,19 @@ Page({
     show:true,
     pendList: [
       {
+        i:0,
         content: '准备每'
       },
       {
+        i:1,
         content: '跑步2公里'
       },
       {
+        i:2,
         content: '跑步2公里'
       },
       {
+        i:3,
         content: '跑步2公里'
       }
     ],
@@ -30,30 +34,54 @@ Page({
     this.setData({
       i:i
     })
-
   },
-  select: function(e) {
-    console.log(e)
-    this.setData({
-    })
+  getContent:function(e){
+    console.log(e.detail.value)
   },
   toShow:function(){
     var List = this.data.pendList;
-    List.unshift({content:'未定义'})
-    console.log(List);
-    console.log(this.data.pendList);
+    for(let j =0;j<List.length;j++){
+      List[j].i = List[j].i+1
+      console.log(List[j])
+    }
+    List.unshift({content:'未定义',i:0})
+    console.log(List)
     this.setData({
       pendList:List
     })
   },
-  commitValue:function(event){
-    console.log(event.detail.value)
+  commitValue:function(e){
+    var i = e.target.dataset.id;
     var List = this.data.pendList;
-    List.unshift(
-      {content:event.detail.value}
-    )
+    for(let i = 0; i<List.length;i++){
+      let j = List[i].i
+      List[i].i = j++
+    }
+    console.log(List)
+    console.log(e.detail.value)
+    List[i].content=e.detail.value;
+    // List[i].content = e.detail.value;
     this.setData({
-      pendList:List
+        pendList:List
+    })
+    wx.cloud.init()
+    wx.cloud.callFunction({
+      name:"addTodos",
+      data:{
+        things:e.detail.value
+      },
+      success:function(res){
+        console.log(res.result)
+      }
+    })
+    wx.cloud.callFunction({
+      name: "todos",
+      // data: {
+      //   things: e.detail.value
+      // },
+      success: function (res) {
+        console.log(res.result)
+      }
     })
   }
 })
