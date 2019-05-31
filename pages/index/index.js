d82cb8d75f22064b2dd94fe018abae6fb6a1cf8a
CaiyueDamowang <www.caiyue@qq.com>
2019-05-31 18:31:45 +0800

    待办逻辑完成！

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,34 +4,61 @@ const app = getApp();
 const db = wx.cloud.database()
 Page({
   data: {
+    hidden:true,
+    newtip:'',
     i:0,
     show:true,
     pendList: [
     ],
+    form_value:'',
     bgBar: 'bgBar',
     punchedBar: 'punchedBar'
   },
   onLoad: function(){
     this.findData();
   },
+  //提交事件
+  // 按钮确认
+  submit:function(){
+    let value = this.data.newtip;
+    if(value !== ''){
+      db.collection('todos').add({
+        data: {
+          things: value,
+        },
+        success:function(res){
+          console.log(res)
+        }
+      })
+      this.setData({
+        hidden: true
+      })
+      this.findData();
+    }
+  },
+  //按钮取消
+  cancle:function(){
+    data: {
+      hidden: true
+    }
+  },
+  //失去焦点
   getContent:function(e){
-    console.log(e.detail.value)
+    let value  = e.detail.value;
+    
+    this.setData({
+      newtip:value
+    })
+    console.log(this.data.newtip);
   },
   toShow:function(){
     //点击事件
-    var List = this.data.pendList;
-    List.unshift({
-      content:'请输入'
-    })
-    for(let j = 0; j<List.length;j++){
-      List[j].i = j
-    }
+    let hidden = false;
     this.setData({
-      pendList:List
+      hidden:hidden
     })
   },
   findData: function(){
-
     let openId = wx.getStorageSync("openId");
     // console.info(openId);
     db.collection('todos').where({
@@ -39,8 +66,12 @@ Page({
     }).get({
       success: res => {
         console.log(res.data);
+        let reverse = []
+        for(let i = 0;i<res.data.length;i++){
+          reverse.unshift(res.data[i])
+        }
         this.setData({
-          pendList: res.data
+          pendList: reverse
         })
 
         // for(let i = 0; i < res.data.length;i++){
@@ -52,36 +83,37 @@ Page({
       }
     });
   },
-  commitValue:function(e){
-    console.info(e);
-    if(e.detail.value == "")return;
-    var i = e.target.dataset.id;
-    console.log(i)
-    var List = this.data.pendList;
-    console.log(List)
-    this.setData({
-      pendList:List
-    });
+  //失去焦点
+  // commitValue:function(e){
+  //   console.info(e);
+  //   if(e.detail.value == "")return;
+  //   var i = e.target.dataset.id;
+  //   console.log(i)
+  //   var List = this.data.pendList;
+  //   console.log(List)
+  //   this.setData({
+  //     pendList:List
+  //   });
     
-    db.collection('todos').add({
-      // data 字段表示需新增的 JSON 数据
-      data: {
-        things:e.detail.value
-      },
-      success: function (res) {
-        let data = this.data.pendList;
-        data.push({
-          '_id':res._id,
-          'things':e.detail.value
-        });
-        this.setData({
-          pendList:data
-        })
-        // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
-        console.log(res)
-      }
-    });
-  }
+  //   db.collection('todos').add({
+  //     // data 字段表示需新增的 JSON 数据
+  //     data: {
+  //       things:e.detail.value
+  //     },
+  //     success: function (res) {
+  //       let data = this.data.pendList;
+  //       data.push({
+  //         '_id':res._id,
+  //         'things':e.detail.value
+  //       });
+  //       this.setData({
+  //         pendList:data
+  //       })
+  //       // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+  //       console.log(res)
+  //     }
+    // });
+  // }
   
 })
   
