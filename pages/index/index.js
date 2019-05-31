4e27188d8135224490e1ecc8adb6b3207ffb9b06
CaiyueDamowang <www.caiyue@qq.com>
2019-05-31 17:35:52 +0800

    数据库

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,43 +7,12 @@ Page({
     i:0,
     show:true,
     pendList: [
-      {
-        i:0,
-        content: '准备每'
-      },
-      {
-        i:1,
-        content: '跑步2公里'
-      },
-      {
-        i:2,
-        content: '跑步2公里'
-      },
-      {
-        i:3,
-        content: '跑步2公里'
-      }
     ],
     bgBar: 'bgBar',
     punchedBar: 'punchedBar'
   },
   onLoad: function(){
-
-    let openId = wx.getStorageSync("openId");
-    // console.info(openId);
-    db.collection('todos').where({
-      open_id: openId
-    }).get({
-      success:res=>{
-        console.log(res.data);
-        for(let i = 0; i < res.data.length;i++){
-          res.data[i].content = res.data[i].things[0].content
-        }
-        this.setData({
-          pendList:res.data
-        })
-      }
-    })
+    this.findData();
   },
   getContent:function(e){
     console.log(e.detail.value)
@@ -61,28 +30,59 @@ Page({
       pendList:List
     })
   },
+  findData: function(){
+
+    let openId = wx.getStorageSync("openId");
+    // console.info(openId);
+    db.collection('todos').where({
+      _openid: openId
+    }).get({
+      success: res => {
+        console.log(res.data);
+        this.setData({
+          pendList: res.data
+        })
+
+        // for(let i = 0; i < res.data.length;i++){
+        //   res.data[i].content = res.data[i].things[0].content
+        // }
+        // this.setData({
+        //   pendList:res.data
+        // })
+      }
+    });
+  },
   commitValue:function(e){
+    console.info(e);
+    if(e.detail.value == "")return;
     var i = e.target.dataset.id;
     console.log(i)
     var List = this.data.pendList;
     console.log(List)
     this.setData({
       pendList:List
-    })
-    wx.cloud.callFunction({
-      // 云函数名称
-      name: 'addTodos',
-      // 传给云函数的参数
-      data: {
-        things: this.data.pendList
-      },
-    })
-      .then(res => {
-        console.log(this.data.pendList) // 3
-      })
-      .catch(console.error)
+    });
     
+    db.collection('todos').add({
+      // data 字段表示需新增的 JSON 数据
+      data: {
+        things:e.detail.value
+      },
+      success: function (res) {
+        let data = this.data.pendList;
+        data.push({
+          '_id':res._id,
+          'things':e.detail.value
+        });
+        this.setData({
+          pendList:data
+        })
+        // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+        console.log(res)
+      }
+    });
   }
+  
 })
   
 
